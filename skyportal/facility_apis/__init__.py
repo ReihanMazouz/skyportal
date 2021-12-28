f87dca8a53e8e8ba3543ebc73082ddc7ecdd5439
Michael Coughlin <michael.w.coughlin@gmail.com>
2021-12-28 15:19:41 -0600

    ATLAS forced photometry

--- skyportal/facility_apis/__init__.py
+++ skyportal/facility_apis/__init__.py
@@ -3,3 +3,4 @@ from .sedm import SEDMAPI, SEDMListener
 from .lt import IOOAPI, IOIAPI, SPRATAPI
 from .lco import SINISTROAPI, SPECTRALAPI, FLOYDSAPI, MUSCATAPI
 from .ztf import ZTFAPI
+from .atlas import ATLASAPI
